3a4854fdcff77d8e98942fdf2887c8cb7c409f0d
BEAN <irenelemon00@naver.com>
2023-06-07 18:48:07 +0900

    checking blank text

--- pages/add_verified_restaurants.py
+++ pages/add_verified_restaurants.py
@@ -8,8 +8,9 @@ costRange = st.radio(label='1만원 이하 가격일까요 아니면 2만원 이
 st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
 def on_button_click(name, menu) :
-    if (name or menu != 0) :
+    if (name or menu != '') :
         st.balloons()
+        st.success('식당 추가가 완료되었습니다 🥳')
     else :
         st.error('아직 작성되지 않은 부분이 있습니다. 모든 입력란을 작성해주세요 🙏')
 
@@ -17,8 +18,6 @@ if costRange == '1만원 이하':
     nomalName = st.text_input('식당 이름을 입력해주세요 ❗️', placeholder = '식당 명')
     nomalMenu = st.text_input('어떤 메뉴가 맛있나요 ❓', placeholder = '추천 메뉴')
     nomalDistance = st.radio(label='회사랑 어느 정도로 떨어져있나요 ❓', options=['5분 컷', '10분 컷', '15분 이상 🥲'])
-    # st.write('nomalName: ',nomalName)
-    # st.write(nomalMenu)
     if st.button("작성 완료"):
         on_button_click(nomalName, nomalMenu)
 
