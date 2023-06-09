59e93272983e97fbac59f0f2e1b5e0b76aecfbba
BEAN <irenelemon00@naver.com>
2023-06-09 17:41:07 +0900

    add gitignore file

--- main_page.py
+++ main_page.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import openpyxl
-from sklearn.feature_extraction.text import TfidfVectorizer
+from openpyxl import Workbook
 
 st.set_page_config(page_title="관악구 맛집 지도", page_icon="🍴")
 st.title('관악구 맛집 지도 🍽️')
@@ -10,57 +9,61 @@ st.title('관악구 맛집 지도 🍽️')
 with st.sidebar:
     st.text_input("식당 이름 검색 🔍")
 
+rawVisitedPlacesdf = pd.read_excel("./visitedRestaurant.xlsx", skiprows = 1)
+sortedVisitedPlacesdf = rawVisitedPlacesdf.sort_values(by = '업체명', ascending = False)
+sortedVisitedPlacesdf['업체명'] = sortedVisitedPlacesdf['업체명'].str.replace(pat=' ', repl = '')
+removedWhiteSpaceVisitedPlacesdf = sortedVisitedPlacesdf.copy()
+removedWhiteSpaceVisitedPlacesdf.insert(2, '방문 횟수', 1)
+nonCleanedVisitedPlacesdf = removedWhiteSpaceVisitedPlacesdf
+nonCleanedVisitedPlacesdf = nonCleanedVisitedPlacesdf.reset_index(drop=True)
+for count in range(len(nonCleanedVisitedPlacesdf) - 1) :
+    if nonCleanedVisitedPlacesdf['업체명'][count + 1] in nonCleanedVisitedPlacesdf['업체명'][count] :
+        nonCleanedVisitedPlacesdf['업체명'][count + 1] = nonCleanedVisitedPlacesdf['업체명'][count]
+nonCleanedVisitedPlacesdf = nonCleanedVisitedPlacesdf.sort_values(by = '업체명',ascending = True)
+nonCleanedVisitedPlacesdf = nonCleanedVisitedPlacesdf.reset_index(drop=True)
+dfLen = len(nonCleanedVisitedPlacesdf) - 1
+for count in range(dfLen) :
+    if nonCleanedVisitedPlacesdf['업체명'][count + 1] == nonCleanedVisitedPlacesdf['업체명'][count] :
+        nonCleanedVisitedPlacesdf['사용금액'][count + 1] += nonCleanedVisitedPlacesdf['사용금액'][count]
+        nonCleanedVisitedPlacesdf['방문 횟수'][count + 1] += nonCleanedVisitedPlacesdf['방문 횟수'][count]
+nonCleanedVisitedPlacesdf = nonCleanedVisitedPlacesdf.sort_values(by = '업체명',ascending = False)
+nonCleanedVisitedPlacesdf = nonCleanedVisitedPlacesdf.reset_index(drop=True)
+visitedPlacesdf = nonCleanedVisitedPlacesdf.drop_duplicates(['업체명'])
+visitedPlacesdf = visitedPlacesdf.sort_values(by = '업체명',ascending = True)
+cleanedVisitedPlacesdf = visitedPlacesdf.reset_index(drop=True)
+
 tab1, tab2, tab3 = st.tabs(['검증된 맛집 모음', '식당 방문 이력','모범음식점 모음'])
 
 
 with tab1:
     st.subheader('검증된 맛집 리스트 ✅')
 
-    with st.expander('월수목금 기준 (10000₩)'):
-        normalVerifiedRestaurantsdf = pd.DataFrame(columns=['식당 명','추천 메뉴', '거리'])
-        st.write(normalVerifiedRestaurantsdf)
+    with st.expander('월수목금 기준 (₩10000)'):
+        # normalVerifiedRestaurantsdf = pd.DataFrame(columns=['식당 명','추천 메뉴', '거리', '삭제'])
+        # st.write(normalVerifiedRestaurantsdf)
+        normalVerifiedRestaurantsdf = pd.DataFrame(columns=['식당 명','추천 메뉴', '거리', '삭제'])
+        
+        testdf = pd.DataFrame({
+            '식당 명': ['테스트1', '테스트2', '테스트3'],
+            '추천 메뉴': ['메뉴1', '메뉴2', '메뉴3'],
+            '거리': ['5분', '10분', '15분']
+        })
+        st.dataframe(testdf)
 
-    with st.expander('화요일 기준 (20000₩)'):
-        specialVerifiedRestaurantsdf = pd.DataFrame(columns=['식당 명','추천 메뉴', '거리'])
+
+    with st.expander('화요일 기준 (₩20000)'):
+        specialVerifiedRestaurantsdf = pd.DataFrame(columns=['식당 명','추천 메뉴', '거리', '삭제'])
         st.write(specialVerifiedRestaurantsdf)
     
 with tab2:
-    st.header('월수목금 기준 (10000₩)')
+    # st.header('월수목금 기준 (10000₩)')
     st.subheader('가장 많이 방문한 식당 Top 5 👣')
+    st.write((cleanedVisitedPlacesdf.sort_values(by = '방문 횟수', ascending = False).reset_index(drop=True))[0:5])
     st.subheader('가장 많이 결제한 식당 Top 5 🤑')
-    with st.expander('방문했던 식당 리스트 ✍️'):
+    st.write((cleanedVisitedPlacesdf.sort_values(by = '사용금액', ascending = False).reset_index(drop=True))[0:5])
     
-        rawVisitedPlacesdf = pd.read_excel("./visitedRestaurant.xlsx", skiprows = 1)
-        sortedVisitedPlacesdf = rawVisitedPlacesdf.sort_values(by = '업체명', ascending = False)
-        sortedVisitedPlacesdf['업체명'] = sortedVisitedPlacesdf['업체명'].str.replace(pat=' ', repl = '')
-        removedWhiteSpaceVisitedPlacesdf = sortedVisitedPlacesdf.copy()
-        removedWhiteSpaceVisitedPlacesdf.insert(2, '방문 횟수', 1)
-        nonCleanedVisitedPlacesdf = removedWhiteSpaceVisitedPlacesdf
-        nonCleanedVisitedPlacesdf = nonCleanedVisitedPlacesdf.reset_index(drop=True)
-        for count in range(len(nonCleanedVisitedPlacesdf) - 1) :
-            if nonCleanedVisitedPlacesdf['업체명'][count + 1] in nonCleanedVisitedPlacesdf['업체명'][count] :
-                nonCleanedVisitedPlacesdf['업체명'][count + 1] = nonCleanedVisitedPlacesdf['업체명'][count]
-        nonCleanedVisitedPlacesdf = nonCleanedVisitedPlacesdf.sort_values(by = '업체명',ascending = True)
-        nonCleanedVisitedPlacesdf = nonCleanedVisitedPlacesdf.reset_index(drop=True)
-        dfLen = len(nonCleanedVisitedPlacesdf) - 1
-        for count in range(dfLen) :
-            if nonCleanedVisitedPlacesdf['업체명'][count + 1] == nonCleanedVisitedPlacesdf['업체명'][count] :
-                nonCleanedVisitedPlacesdf['사용금액'][count + 1] += nonCleanedVisitedPlacesdf['사용금액'][count]
-                nonCleanedVisitedPlacesdf['방문 횟수'][count + 1] += nonCleanedVisitedPlacesdf['방문 횟수'][count]
-        nonCleanedVisitedPlacesdf = nonCleanedVisitedPlacesdf.sort_values(by = '업체명',ascending = False)
-        nonCleanedVisitedPlacesdf = nonCleanedVisitedPlacesdf.reset_index(drop=True)
-        visitedPlacesdf = nonCleanedVisitedPlacesdf.drop_duplicates(['업체명'])
-        visitedPlacesdf = visitedPlacesdf.sort_values(by = '업체명',ascending = True)
-        cleanedVisitedPlacesdf = visitedPlacesdf.reset_index(drop=True)
+    with st.expander('방문했던 식당 리스트 ✍️'):
         st.write(cleanedVisitedPlacesdf)
-        # for countColumnNum in range(len(visitedPlacesdf)):
-            # for nestedCountColumnNum in range(len(rawVisitedPlacesdf)):
-        # st.write(visitedPlacesdf.drop_duplicates(['업체명'], keep='first'))
-                # nestedCountColumnNum += 1
-            # countColumnNum +=1
-                # if visitedPlacesdf['업체명'].iloc[countColumnNum] == rawVisitedPlacesdf['업체명'].iloc[nestedCountColumnNum]:
-                    # visitedPlacesdf.loc[countColumnNum, '방문 횟수'] += 1
-
 
 with tab3:
 
@@ -92,7 +95,7 @@ with tab3:
             (modelRestaurantdf['소재지도로명'].str.contains('쑥고개로')) |
             (modelRestaurantdf['소재지도로명'].str.contains('관악로')) |
             (modelRestaurantdf['소재지도로명'].str.contains('남부순환로'))
-]
+        ]
     elif select_near_place == '신림':
         tmpdf = modelRestaurantdf[modelRestaurantdf['소재지도로명'].str.contains('신림')]
     elif select_near_place == '봉천' :
@@ -111,14 +114,3 @@ with tab3:
 
     
     st.write(tmpdf)
-    with st.form("my_form"):
-        st.write("Inside the form")
-        slider_val = st.slider("Form slider")
-        checkbox_val = st.checkbox("Form checkbox")
-
-   # Every form must have a submit button.
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-           st.write("slider", slider_val, "checkbox", checkbox_val)
-
-    st.write("Outside the form")
--- pages/add_verified_restaurants.py
+++ pages/add_verified_restaurants.py
@@ -5,12 +5,10 @@ import time
 from streamlit_extras.switch_page_button import switch_page
 
 st.header('검증된 맛집을 추가해주세요')
+st.info('행을 잘못 추가한 경우, 제일 앞의 체크박스를 누르고 del키를 누르면 해당 행이 삭제됩니다')
 
-costRange = st.radio(label='1만원 이하 가격일까요 아니면 2만원 이하 가격일까요 ❓', options=['1만원 이하', '2만원 이하'])
-st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-
-def on_button_click(name, menu) :
-    if (name or menu != '') :
+def finish_button_click(df) :
+    if (df.isnul) :
         st.balloons()
         st.success('식당 추가가 완료되었습니다 🥳 메인 홈페이지로 이동합니다')
         time.sleep(2.5)
@@ -18,14 +16,63 @@ def on_button_click(name, menu) :
     else :
         st.error('아직 작성되지 않은 부분이 있습니다. 모든 입력란을 작성해주세요 🙏')
 
-if costRange == '1만원 이하':
-    normalName = st.text_input('식당 이름을 입력해주세요 ❗️', placeholder = '식당 명')
-    normalMenu = st.text_input('어떤 메뉴가 맛있나요 ❓', placeholder = '추천 메뉴')
-    normalDistance = st.radio(label='회사랑 어느 정도로 떨어져있나요 ❓', options=['5분 컷', '10분 컷', '15분 이상 🥲'])
-    if st.button("작성 완료"):
-        on_button_click(normalName, normalMenu)
+normalVerifiedRestaurantsdf = pd.DataFrame(columns=['식당 명','추천 메뉴', '거리', '별점', '한 줄 코멘트'])
 
-else:
-    specialName = st.text_input('식당 이름을 입력해주세요 ❗️', placeholder = '식당 명')
-    specialMenu = st.text_input('어떤 메뉴가 맛있나요 ❓', placeholder = '추천 메뉴')
-    specialDistance = st.radio(label='회사랑 어느 정도로 떨어져있나요 ❓', options=['5분 컷', '10분 컷', '15분 이상 🥲'])
+modifiedNormalVerifiedRestaurantsdf = st.data_editor(
+    normalVerifiedRestaurantsdf,
+    column_config = {
+        '식당 명': '식당 명',
+        '추천 메뉴': '추천 메뉴',
+        '거리': st.column_config.NumberColumn(
+            '거리',
+            help = '회사에서 도보 기준으로 얼마나 걸리나요? (분 만 입력해주세요)',
+            min_value = 0,
+            max_value = 15,
+            step = 5,
+            format = '%d 분 이상',
+            required = True
+        ),
+        '별점': st.column_config.NumberColumn(
+            "Your rating",
+            help="How much do you like this command (1-5)?",
+            min_value=1,
+            max_value=5,
+            step=1,
+            format="%d ⭐",
+            required = True
+        )
+    },
+    num_rows='dynamic',
+
+    
+)
+
+st.write('test')
+st.write(pd.isnull(modifiedNormalVerifiedRestaurantsdf))
+
+if st.button("작성 완료"):
+    finish_button_click(modifiedNormalVerifiedRestaurantsdf)
+
+# costRange = st.radio(label='1만원 이하 가격일까요 아니면 2만원 이하 가격일까요 ❓', options=['1만원 이하', '2만원 이하'])
+# st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+
+# def on_button_click(name, menu) :
+#     if (name or menu != '') :
+#         st.balloons()
+#         st.success('식당 추가가 완료되었습니다 🥳 메인 홈페이지로 이동합니다')
+#         time.sleep(2.5)
+#         switch_page("main_page")
+#     else :
+#         st.error('아직 작성되지 않은 부분이 있습니다. 모든 입력란을 작성해주세요 🙏')
+
+# if costRange == '1만원 이하':
+#     normalName = st.text_input('식당 이름을 입력해주세요 ❗️', placeholder = '식당 명')
+#     normalMenu = st.text_input('어떤 메뉴가 맛있나요 ❓', placeholder = '추천 메뉴')
+#     normalDistance = st.radio(label='회사랑 어느 정도로 떨어져있나요 ❓', options=['5분 컷', '10분 컷', '15분 이상 🥲'])
+#     if st.button("작성 완료"):
+#         on_button_click(normalName, normalMenu)
+
+# else:
+#     specialName = st.text_input('식당 이름을 입력해주세요 ❗️', placeholder = '식당 명')
+#     specialMenu = st.text_input('어떤 메뉴가 맛있나요 ❓', placeholder = '추천 메뉴')
+#     specialDistance = st.radio(label='회사랑 어느 정도로 떨어져있나요 ❓', options=['5분 컷', '10분 컷', '15분 이상 🥲'])
